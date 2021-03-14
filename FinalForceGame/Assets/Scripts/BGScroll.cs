4de83eed49acb88a57130779c150ef91b2b4c915
Sabeeh <muhammadsabeeh01@gmail.com>
2021-03-14 18:26:47 -0400

    Third Commit
    game is infinite

---
+++ FinalForceGame/Assets/Scripts/BGScroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    public float speed = 4f; // scrolling speed
+    private Vector3 StartPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        if (transform.position.y < -10f)
+        {
+            transform.position = StartPos;
+        }
+    }
+}
