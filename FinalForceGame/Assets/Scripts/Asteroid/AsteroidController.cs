f42fa805077b31baed361e3fd023bc80f0f7c3d2
Sabeeh <muhammadsabeeh01@gmail.com>
2021-03-19 14:28:00 -0400

    Tenth Commit
    Added difficulty to boss (Asteroids spawn in while fighting boss)
    Minor bug fixes

---
+++ FinalForceGame/Assets/Scripts/Asteroid/AsteroidController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    // Update is called once per frame
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        for (int i = 0; i<hazardCount; i++ )
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(hazard, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
